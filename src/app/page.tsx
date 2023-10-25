008985c3aab3e6bead975bc2ef2eb8ee836a9413
fabba1892 <fabian.vanwyk@gmail.com>
2023-10-25 20:41:37 +0200

    landing page h1 and colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  return  <Button>CLick ME!</Button>;
+  return  (
+  <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to to-teal-100" >
+    <div className="absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <h1 className="font-semibold text-7xl text-center">
+        AI <span className="text-green-200 font-bold">note taking</span> assistant
+      </h1>
+      <div className="mt-4"></div>
+      <h2 className="font-semibold text-3xl text-center text-slate-700">
+        Ai Powered
+      </h2>
+    </div>
+  </div>
+  )
 }
