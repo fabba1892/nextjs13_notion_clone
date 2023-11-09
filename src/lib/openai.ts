c5dac68681e8383b8b213a842f11a7bcaced9c46
fabba1892 <fabian.vanwyk@gmail.com>
2023-11-09 20:52:52 +0200

    code updated for the propmt to dall E

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,7 +1,27 @@
-export async function generateImagePrompt(prompt:string) {
-    //math
+import {Configuration, OpenAIApi} from 'openai-edge'
 
-    // -> thumbnail has a bunch of colorfuyl numbers 
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(config);
+export async function generateImagePrompt(name:string) {
+    try {
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{
+                role: 'system',
+                content: 'You are a creative an dhelpful AI assistant capable of generating interesting thumbnail descriptions for my notes. Your output will be fed into the DALLE API to generate a thumbnail. The description should be minimalistic and flat styled',
+            },
+            {
+                role: 'user',
+                content: `Please generate a thumbnail for my notebook titles ${name}`,
+            },
+           ],
+        });
+    } catch (error) {
+        
+    }
 }
 
 export async function generateImage() {
